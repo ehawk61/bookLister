712cfc3b5507735bdc827cf42db9fd1021072147
Jonathan Meek <jonathan.l.meek@gmail.com>
2025-03-08 08:54:08 -0500

    additional work on dev environment

--- development.nix
+++ development.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ sqlite sqlitebrowser ];
+    nativeBuildInputs = with pkgs.buildPackages; [ sqlite sqlitebrowser redocly jdk_headless maven ];
 }

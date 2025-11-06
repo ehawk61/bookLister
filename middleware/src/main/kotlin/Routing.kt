a1fda4f1e8d2b6e406b859147f41a6dead9d675e
Jonathan L Meek <jonathan.l.meek@gmail.com>
2025-11-05 21:03:18 -0500

    feat: working healthcheck w/ a single unit test

--- middleware/src/main/kotlin/Routing.kt
+++ middleware/src/main/kotlin/Routing.kt
@@ -1,18 +1,27 @@
 package es.meeknot
 
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
+    install(StatusPages) {
+        exception<IllegalStateException> { call, cause ->
+            call.respondText("App in illegal state as ${cause.message}")
         }
-        // Static plugin. Try to access `/static/index.html`
-        staticResources("/static", "static")
+    }
+    routing {
+        get("/healthcheck") {
+            val apiVersionValue = call.request.headers["X-API-Version"]
+            when (apiVersionValue) {
+                "v1" -> call.respond(HttpStatusCode.OK, mapOf("apiVersion" to apiVersionValue, "status" to "UP"))
+                else -> call.respond(HttpStatusCode.OK, mapOf("apiVersion" to "Undefined Version", "status" to "UP"))
+            }
+        }
     }
 }
--- middleware/src/test/kotlin/ApplicationTest.kt
+++ middleware/src/test/kotlin/ApplicationTest.kt
@@ -1,21 +1,37 @@
 package es.meeknot
 
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class HealthcheckResponse(val apiVersion: String, val status: String)
 
 class ApplicationTest {
 
     @Test
     fun testRoot() = testApplication {
-        application {
-            module()
+        application { module() }
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
         }
-        client.get("/").apply {
+        client.get("/healthcheck").apply {
             assertEquals(HttpStatusCode.OK, status)
+            runBlocking {
+                val expectedRepsonse = HealthcheckResponse(apiVersion = "Undefined Version", status = "UP")
+                val acutalResponse = body<HealthcheckResponse>()
+                assertEquals(expectedRepsonse, acutalResponse)
+            }
         }
     }
-
 }
